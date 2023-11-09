7b2a90bbf9e5aa592df45dd6f5c6a3a2e033b90a
Lautaro <lautaronalajaime@gmail.com>
2023-11-09 13:10:59 -0300

    V. 73745 0.0

--- src/app/transacciones/transacciones.component.ts
+++ src/app/transacciones/transacciones.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { WalletAPIService } from '../wallet-api.service';
 
 @Component({
   selector: 'app-transacciones',
   templateUrl: './transacciones.component.html',
   styleUrls: ['./transacciones.component.css']
 })
-export class TransaccionesComponent {
 
+export class TransaccionesComponent {
+  tipoTransaccion: string = 'INGRESO';
+
+  montoIngreso: number=0;
+  descripcionIngreso: string="";
+  cuentaIngreso: string="";
+
+  montoGasto: number=0;
+  descripcionGasto: string="";
+  categoriaGasto: string="";
+  cuentaGasto: string="";
+
+  constructor(private walletService: WalletAPIService) {}
+
+  onTipoTransaccionChange(tipo: string): void {
+    this.tipoTransaccion = tipo;
+  }
+
+  realizarIngreso(): void {
+    const ingresoData = {
+      monto: this.montoIngreso,
+      descripcion: this.descripcionIngreso,
+      cuenta: this.cuentaIngreso
+    };
+
+    this.walletService.sumarBalance(idWallet, ingresoData).subscribe(
+      (respuesta) => {
+        console.log('Balance sumado con éxito', respuesta);
+        // Realiza acciones adicionales si es necesario
+      },
+      (error) => {
+        console.error('Error al sumar el balance', error);
+        // Maneja el error de acuerdo a tus necesidades
+      }
+    );
+  }
+
+  realizarGasto(): void {
+    const gastoData = {
+      monto: this.montoGasto,
+      descripcion: this.descripcionGasto,
+      categoria: this.categoriaGasto,
+      cuenta: this.cuentaGasto
+    };
+
+    this.walletService.restarBalance(idWallet, gastoData).subscribe(
+      (respuesta) => {
+        console.log('Balance restado con éxito', respuesta);
+        // Realiza acciones adicionales si es necesario
+      },
+      (error) => {
+        console.error('Error al restar el balance', error);
+        // Maneja el error de acuerdo a tus necesidades
+      }
+    );
+  }
 }
--- src/app/wallet-api.service.ts
+++ src/app/wallet-api.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'; 
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class WalletAPIService {
+
+  private apiUrl = 'https://wallet37.p.rapidapi.com/wallet';
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'X-RapidAPI-Key': '14a24bc25dmsh3c1d1ae71c1dab5p1077f7jsn0a6478e24c6f',
+    'X-RapidAPI-Host': 'wallet37.p.rapidapi.com',
+  });
 
   constructor(private http: HttpClient) {}
 
@@ -13,8 +21,84 @@ export class WalletAPIService {
     return this.http.get('https://ejemplo.com/api/datos');
   }
 
-  // Método para enviar datos a la API (POST)
-  enviarDatos(datos: any) {
+  enviarDatos(datos: any): Observable<any> {
     return this.http.post('https://ejemplo.com/api/enviar', datos);
   }
+  
+  
+  // Nuevo método para obtener datos del usuario por token
+  getUserByToken(token: string): Observable<any> {
+    const url = `https://wallet37.p.rapidapi.com/user/token/${token}`;
+
+    return this.http.get(url, { headers: this.headers });
+  }
+
+   // Nuevo método para obtener datos del usuario por ID
+   getUserByID(userId: string): Observable<any> {
+    const url = `https://wallet37.p.rapidapi.com/user/${userId}`;
+
+    return this.http.get(url, { headers: this.headers });
+  }
+
+  // Nuevo método para iniciar sesión
+  iniciarSesion(email: string, password: string): Observable<any> {
+    const url = 'https://wallet37.p.rapidapi.com/user/sign-in';
+    const data = { email, password };
+
+    return this.http.post(url, data, { headers: this.headers });
+  }
+  // Nuevo método para crear una wallet
+  crearWallet(walletData: any): Observable<any> {
+    return this.http.post(this.apiUrl, walletData, { headers: this.headers });
+  }
+  
+  // Nuevo método para obtener el balance total del usuario
+  getTotalUserBalance(userId: string): Observable<any> {
+      const url = `https://wallet37.p.rapidapi.com/wallet/total-balance/${userId}`;
+  
+      return this.http.get(url, { headers: this.headers });
+  }
+
+  
+  // Nuevo método para obtener las billeteras del usuario
+  getUserWallets(userId: string): Observable<any> {
+    const url = `${this.apiUrl}/${userId}`;
+
+    return this.http.get(url, { headers: this.headers });
+  }
+
+   // Nuevo método para compartir una billetera
+   shareWallet(walletId: string, userId: string, data: any): Observable<any> {
+    const url = `${this.apiUrl}/share-wallet`;
+    const params = new HttpParams()
+      .set('walletId', walletId)
+      .set('userId', userId);
+
+    return this.http.patch(url, data, { headers: this.headers, params });
+  }
+
+   // Nuevo método para actualizar el saldo de la billetera
+   updateWallet(walletId: string, token: string, value: string, data: any): Observable<any> {
+    const url = `${this.apiUrl}/update-balance`;
+    const params = new HttpParams()
+      .set('walletId', walletId)
+      .set('token', token)
+      .set('value', value);
+
+    return this.http.patch(url, data, { headers: this.headers, params });
+  }
+    // Nuevo método para sumar balance
+    sumarBalance(walletId: string, balanceData: any): Observable<any> {
+      const url = `${this.apiUrl}/${walletId}/plus-balance`;
+  
+      return this.http.patch(url, balanceData, { headers: this.headers });
+    }
+    
+     // Nuevo método para restar balance
+  restarBalance(walletId: string, balanceData: any): Observable<any> {
+    const url = `${this.apiUrl}/${walletId}/minus-balance`;
+
+    return this.http.patch(url, balanceData, { headers: this.headers });
+  }
+
 }
