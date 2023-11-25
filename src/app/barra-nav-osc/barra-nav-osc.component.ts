7569c6be9c84b32bd577f805d8ac05b4dd72ff1b
Lucas Paz <lucasapaz2003@gmail.com>
2023-11-25 15:15:16 -0300

    arreglo de bug modo oscuro

--- src/app/barra-nav-osc/barra-nav-osc.component.ts
+++ src/app/barra-nav-osc/barra-nav-osc.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WalletAPIService } from '../wallet-api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-barra-nav-osc',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./barra-nav-osc.component.css']
 })
 export class BarraNavOScComponent {
+
+constructor(private router: Router, private walletService: WalletAPIService) {}
+
+
 mostrarMenu: boolean = false;
+
+cerrarSesion(): void {
+  // Llama a la función de cerrar sesión del servicio
+  this.walletService.cerrarSesion();
+  this.router.navigate(['/login']);
+}
 
 }
